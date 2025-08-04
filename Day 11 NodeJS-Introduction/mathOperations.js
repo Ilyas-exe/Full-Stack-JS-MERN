44973c9a5a79622c86a06f3db0ed9e4ca12f7ada
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-08-04 11:03:26 +0100

    Add math operations module
    Introduced mathOperations.js with basic arithmetic functions and example usage for Node.js.

---
+++ Day 11 NodeJS-Introduction/mathOperations.js
@@ -0,0 +1,19 @@
+function addNumbers(num1,num2){
+    return num1+num2
+}
+function subNumbers(num1,num2){
+    return num1-num2
+}
+function multiplyNumbers(num1,num2){
+    return num1*num2
+}
+function divideNumbers(num1,num2){
+    return num1/num2
+}
+
+console.log(addNumbers(5,4))
+console.log(subNumbers(5,4))
+console.log(multiplyNumbers(5,4))
+console.log(divideNumbers(5,4))
+
+//Run the command node mathOperations.js
--- Day 7 ES6/digitalRoot.js
+++ Day 7 ES6/digitalRoot.js
@@ -4,17 +4,17 @@ function digitalRoot(n) {
   n = n.toString()
   while (true) {
     sum += Number(n[i])
-    
-    if(i==n.length-1){
-        i=0
-        n = sum+''
-        sum = 0
-        if(n.toString().length==1){break}
-    }else{
-        i++
+
+    if (i == n.length - 1) {
+      i = 0
+      n = sum + ''
+      sum = 0
+      if (n.toString().length == 1) { break }
+    } else {
+      i++
     }
   }
-        
+
   return n
 }
 console.log(digitalRoot(49319))
