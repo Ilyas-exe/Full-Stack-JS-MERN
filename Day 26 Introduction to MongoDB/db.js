96d1875da33e964f5d3034758940ecf93c18e63a
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-08-26 12:01:51 +0100

    Add MongoDB connection and user collection demo
    Introduces a script to connect to a local MongoDB instance, access the 'users' collection in the 'mydb' database, and display all user documents. Useful for basic MongoDB integration and testing.

---
+++ Day 26 Introduction to MongoDB/db.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+
+client
+    .connect()
+    .then(()=>{console.log('connected in port 27017')})
+    .catch((error)=>{console.log(error)})
+
+const db = client.db('mydb')
+const collection = db.collection('users')
+async function showCollection() {
+    const users = await collection.find({}).toArray()
+    console.log(users)
+}
+showCollection()
